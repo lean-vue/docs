ba96f947d340221cd525b474d79078a4f25ae31c
Micha Alt <mail@lean-stack.de>
2022-02-16 23:58:00 +0100

    Initial (empty) content.

---
+++ assets/index.md.e40dbaee.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.44c99ba1.js";const l='{"title":"Home","description":"","frontmatter":{"home":true,"heroText":"Lean Vue","tagline":"Learn Vue.js a lean way"},"headers":[],"relativePath":"index.md"}',r={};function n(o,s,c,i,_,d){return a(),t("div")}var m=e(r,[["render",n]]);export{l as __pageData,m as default};
